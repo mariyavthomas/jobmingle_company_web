b05657bf385f35dd97b8dc7fc34267f81d2a0191
mariyavthomas <mariyavthomas2019@gmail.com>
2024-06-13 17:33:31 -1200

    last

--- lib/presentaion/Postnewjob/Screen/Post_new_job.dart
+++ lib/presentaion/Postnewjob/Screen/Post_new_job.dart
@@ -1,49 +1,190 @@
 import 'package:flutter/material.dart';
+import 'package:job_mingle_web/utils/Dropdownmenu.dart';
+import 'package:job_mingle_web/utils/constlist/listofitem.dart';
 
-class PostNewJob extends StatelessWidget {
-  const PostNewJob({super.key});
+class PostNewJob extends StatefulWidget {
+  PostNewJob({super.key});
 
   @override
+  State<PostNewJob> createState() => _PostNewJobState();
+}
+
+class _PostNewJobState extends State<PostNewJob> {
+ 
+  final TextEditingController menuController = TextEditingController();
+  ListDropDown? selectedMenu;
+  @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    final formkey = GlobalKey<FormState>();
     return Scaffold(
-       appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 10,
-          actions: [
-            Row(
-              children: [
-                ElevatedButton(onPressed: () {
-                 Navigator.push(context, MaterialPageRoute(builder: (context)=>PostNewJob()));
-                }, child: Text("FIND NEW JOB")),
-                SizedBox(
-                  width: 15,
-                ),
-                TextButton(onPressed: () {}, child: Text("FAQ/HELP")),
-                SizedBox(
-                  width: 15,
-                ),
-                TextButton(onPressed: () {}, child: Text("PROFILE")),
-                SizedBox(
-                  width: 15,
-                ),
-               
-                SizedBox(
-                  width: 100,
-                )
-              ],
-            ),
-          ],
-          title: Padding(
-            padding: const EdgeInsets.only(left: 80),
-            child: Text(
-              'Job Mingle',
-              style: TextStyle(color: Colors.blue),
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 10,
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => PostNewJob()));
+                  },
+                  child: Text("FIND NEW JOB")),
+              SizedBox(width: width * 0.02),
+              TextButton(onPressed: () {}, child: Text("FAQ/HELP")),
+              SizedBox(width: width * 0.02),
+              TextButton(onPressed: () {}, child: Text("PROFILE")),
+              SizedBox(width: width * 0.02),
+
+              // SizedBox(
+              //   width: width * 0.1,
+              // )
+            ],
+          ),
+        ],
+        title: Padding(
+          padding: const EdgeInsets.only(left: 80),
+          child: Text(
+            'Job Mingle',
+            style: TextStyle(color: Colors.blue),
           ),
         ),
-        body: Column(children: [
-          Card()
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(
+            height: height * 0.09,
+          ),
+          Center(
+            child: Container(
+              height: height * 0.3,
+              width: width * 0.7,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 181, 204, 245),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: height * 0.01,
+                  ),
+                  Text(
+                    'Post your job in just 2 minuts!',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ClipRRect(
+                    child: Card(
+                        elevation: 50,
+                        child: Image.asset(
+                          'lib/assets/image/jobmingleimage1.jpg',
+                        )),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: height * 0.1,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40, bottom: 10),
+            child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Basic Job Details",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
+                )),
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: width * 0.06,
+              ),
+              SizedBox(
+                height: height * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: height * 0.6,
+                      width: width * 0.5,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: const Offset(
+                                5.0,
+                                5.0,
+                              ),
+                              blurRadius: 10.0,
+                              spreadRadius: 2.0,
+                            ), //BoxShadow
+                            BoxShadow(
+                              color: Colors.white,
+                              offset: const Offset(0.0, 0.0),
+                              blurRadius: 0.0,
+                              spreadRadius: 0.0,
+                            ), //BoxShadow
+                          ]),
+                      child: Form(
+                        key: formkey,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Job Title *"),
+                              SizedBox(
+                                height: height * 0.01,
+                              ),
+                              SizedBox(
+                                width: width * 0.8,
+                                child: Dropdownmenu(width,
+                                    menuController,"Job Title ",
+                                    selectedMenu,
+                                    jobtitle
+                                ),
+                              ),
+                              SizedBox(
+                                width: width * 0.8,
+                                child: Dropdownmenu(width,
+                                    menuController,"Job Location",
+                                    selectedMenu,
+                                    jobtitle
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: width * 0.1,
+              ),
+              ClipRRect(
+                child: Image.asset("lib/assets/image/BlurMobile.png",
+                    height: height * 0.7),
+              )
+            ],
+          ),
         ]),
+      ),
     );
   }
+
+ 
 }
--- lib/utils/customtextform.dart
+++ lib/utils/customtextform.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:job_mingle_web/application/auth_company/auth_company_bloc.dart';
 
-
 // ignore: must_be_immutable
 class CustomTextFormField extends StatelessWidget {
   final bool readOnly;
@@ -13,15 +12,17 @@ class CustomTextFormField extends StatelessWidget {
 
   final String hintText;
   final bool obscureText;
-
+  final double? width;
+  final double? height;
   final InputBorder? enabledBorder;
   final InputBorder? focusedBorder;
   final TextStyle? errorStyle;
   final Widget? prefixIcon;
   final Widget? suffixIcon;
   final prefixtext;
- // final type;
- final keyboard;
+  // final type;
+  final keyboard;
+  final BorderRadius? borderRadius;
   final TextStyle? style;
   final maxlines;
   final String labeltext;
@@ -34,10 +35,13 @@ class CustomTextFormField extends StatelessWidget {
     this.errorStyle,
     this.readOnly = false,
     this.style,
+    this.borderRadius,
+    this.width,
+    this.height,
     required this.controller,
     this.maxlines = 1,
     //this.type = TextInputType.text,
-     this.keyboard,
+    this.keyboard,
     required this.hintText,
     this.obscureText = false,
     this.enabledBorder,
@@ -56,17 +60,16 @@ class CustomTextFormField extends StatelessWidget {
         if (state is ObsecurecompanyState) {
           _obscureText = state.obscure;
         }
-        return Column(
-          children: [
-            TextFormField(
-              
+        return SizedBox(
+          height: height,
+          width: width,
+          child: TextFormField(
               readOnly: readOnly,
               style: Theme.of(context).textTheme.bodySmall,
               maxLines: maxlines,
               keyboardType: keyboard,
               controller: controller,
               decoration: InputDecoration(
-                
                 labelText: labeltext,
                 errorStyle: TextStyle(color: Colors.red),
                 hintStyle: Theme.of(context).textTheme.bodySmall,
@@ -76,7 +79,6 @@ class CustomTextFormField extends StatelessWidget {
                     borderRadius: BorderRadius.circular(12),
                     borderSide: BorderSide(color: Colors.black)),
                 focusedBorder: OutlineInputBorder(
-                  
                     borderRadius: BorderRadius.circular(
                       12,
                     ),
@@ -87,28 +89,23 @@ class CustomTextFormField extends StatelessWidget {
                         icon: Icon(
                           _obscureText
                               ? Icons.visibility_off
-                            : Icons.visibility,
+                              : Icons.visibility,
                           color: Colors.black,
                         ),
                         onPressed: () {
-                          BlocProvider.of<AuthCompanyBloc>(context)
-                              .add(ObsecuretextcompanyEvent(obscure: _obscureText));
+                          BlocProvider.of<AuthCompanyBloc>(context).add(
+                              ObsecuretextcompanyEvent(obscure: _obscureText));
                         },
                       )
                     : suffixIcon,
-                   
               ),
-
               obscureText: obscureText && _obscureText,
-              validator:validator,
-              autovalidateMode: AutovalidateMode.onUserInteraction
-              
-            ),
-            SizedBox(
-              height: 10,
-            )
-          ],
+              validator: validator,
+              autovalidateMode: AutovalidateMode.onUserInteraction),
         );
+        // SizedBox(
+        //   height: 10,
+        // );
       },
     );
   }
