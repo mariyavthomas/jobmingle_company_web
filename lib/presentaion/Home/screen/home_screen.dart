7cb71177472cfbc686aaf4e986d662bb6cfcfe75
mariyavthomas <mariyavthomas2019@gmail.com>
2024-08-26 14:08:19 -1200

    home updated

--- lib/presentaion/Home/screen/home_screen.dart
+++ lib/presentaion/Home/screen/home_screen.dart
@@ -1,17 +1,18 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:job_mingle_web/application/Applied_candidate/candidate_bloc.dart';
-import 'package:job_mingle_web/application/candidatestatus/candidatestatus_bloc.dart';
+import 'package:job_mingle_web/application/auth_company/auth_company_bloc.dart';
+
 import 'package:job_mingle_web/application/post_job/postjob_bloc.dart';
-import 'package:job_mingle_web/application/rejected/rejected_candidate_bloc.dart';
-import 'package:job_mingle_web/application/shortlistcan/shortlisted_candidate_bloc.dart';
-import 'package:job_mingle_web/presentaion/home/widgets/appbar.dart';
-import 'package:job_mingle_web/presentaion/home/widgets/body/cutomcolum.dart';
-import 'package:job_mingle_web/presentaion/paymet/screen/payment.dart';
-import 'package:sidebar_with_animation/animated_side_bar.dart';
+import 'package:job_mingle_web/domain/company_model.dart';
+import 'package:job_mingle_web/presentaion/Applicants/screen/allapplicant_screen.dart';
+
+import 'package:job_mingle_web/presentaion/Home/widgets/body/cutomcolum.dart';
+
+import 'package:job_mingle_web/presentaion/Postnewjob/Screen/Post_new_job.dart';
+import 'package:job_mingle_web/presentaion/Profile/screen/profile_screen.dart';
+
 
 class CompanyHomeScreen extends StatefulWidget {
   const CompanyHomeScreen({super.key});
@@ -21,130 +22,115 @@ class CompanyHomeScreen extends StatefulWidget {
 }
 
 class _CompanyHomeScreenState extends State<CompanyHomeScreen> {
-  bool _isPaymentIncomplete = false;
-
   @override
   void initState() {
     super.initState();
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      _checkPaymentStatus(user.uid).then((_) {
-        if (_isPaymentIncomplete) {
-          _showIncompletePaymentAlert();
-        }
-      });
-    }
-
-    // Load other data
-    context.read<CandidateBloc>().add(LoadedCandidateCompany());
-    context.read<ShortlistedCandidateBloc>().add(ShortListLoadeddata());
-    context.read<RejectedCandidateBloc>().add(GetRejectedCandidate());
-    context.read<CandidatestatusBloc>().add(Fetchstatus());
-    context.read<PostjobBloc>().add(FetchJobs());
-  }
-
-  Future<void> _checkPaymentStatus(String companyUid) async {
-    try {
-      DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore
-          .instance
-          .collection('company')
-          .doc(companyUid)
-          .get();
-
-      if (snapshot.exists) {
-        var data = snapshot.data();
-        if (data != null && data.containsKey('payment')) {
-          setState(() {
-            _isPaymentIncomplete = data['payment'] == '';
-          });
-        }
-      }
-    } catch (e) {
-      print("Error fetching payment status: $e");
+      final company = Companymodel(companyuid: user.uid);
+      BlocProvider.of<PostjobBloc>(context).add(FetchJobs());
     }
   }
-
-  void _showIncompletePaymentAlert() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Payment Incomplete'),
-            content: Text('Please complete your payment to continue.'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PaymentScreen(),
-                    ),
-                  );
-                },
-                child: Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    });
-  }
-
+ bool isedit=true;
   @override
   Widget build(BuildContext context) {
+    // ignore: unused_local_variable
     double width1 = MediaQuery.of(context).size.width;
+    // ignore: unused_local_variable
     double height2 = MediaQuery.of(context).size.height;
-
     return SafeArea(
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size(width1 * 0.02, height2 * 0.08),
-          child: Homeappbar(width1: width1),
-        ),
-        body: Row(
-          children: [
-            SideBarAnimated(
-              onTap: (s) {},
-              widthSwitch: 700,
-              mainLogoImage: 'assets/logo.png',
-              sidebarItems: [
-                SideBarItem(
-                  iconSelected: Icons.home_rounded,
-                  iconUnselected: Icons.home_outlined,
-                  text: 'Home',
-                ),
-                SideBarItem(
-                  iconSelected: Icons.account_balance_wallet,
-                  iconUnselected: Icons.account_balance_wallet_outlined,
-                  text: 'Insights',
-                ),
-                SideBarItem(
-                  iconSelected: CupertinoIcons.chart_bar_square_fill,
-                  iconUnselected: CupertinoIcons.chart_bar_square,
-                  text: 'Feature',
-                ),
-                SideBarItem(
-                  iconSelected: Icons.credit_card_rounded,
-                  text: 'Payouts',
-                ),
-                SideBarItem(
-                  iconSelected: Icons.settings,
-                  iconUnselected: Icons.settings_outlined,
-                  text: 'Settings',
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              elevation: 10,
+              actions: [
+                Row(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => PostNewJob(isedit: false,)));
+                        },
+                        child: Text("POST NEW JOB")),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    TextButton(onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>AllApplicant()));
+                    }, child: Text("Applicants")),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Profilemain()));
+                        },
+                        child: Text("PROFILE")),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          sigout(context);
+                        },
+                        child: Text("SIGN  OUT")),
+                    SizedBox(
+                      width: 100,
+                    )
+                  ],
                 ),
               ],
-            ),
-            Expanded(
-              child: Container(
-                width: 1700,
-                decoration: BoxDecoration(color: Colors.blue[50]),
-                child: CutomColum(),
+              title: Padding(
+                padding: const EdgeInsets.only(left: 80),
+                child: Text(
+                  'Job Mingle',
+                  style: TextStyle(color: Colors.blue),
+                ),
               ),
             ),
-          ],
-        ),
-      ),
-    );
+            body: Container(
+                width: 1700,
+                decoration: BoxDecoration(color: Colors.blue[50]),
+                child: CutomColum())));
   }
+
+
+  Future<dynamic> sigout(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirm Sign Out'),
+          content: Text('Are you sure you want to sign out?'),
+          actions:[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+                final authBloc = BlocProvider.of<AuthCompanyBloc>(context);
+                authBloc.add(SignOutCompany());
+
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/login', (route) => false);
+              },
+              child: Text('Sign Out'),
+            ),
+          ],
+        );
+      },
+    );
+  
+
 }
+}
+
